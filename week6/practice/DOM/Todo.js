664c40947298b9be2d28d7672aa2c94c8fd91707
h1gyeon9 <nhk1007@naver.com>
2026-04-27 22:40:48 +0900

    chore: Todo.js 주석 달기

--- week6/practice/DOM/Todo.js
+++ week6/practice/DOM/Todo.js
@@ -3,18 +3,28 @@ import Div from "./Div.js";
 
 class Todo {
     constructor(todo){
-        this.row = new Div('', 'row').node;                            //<div class="row"></div>
+        this.row = new Div('', 'row').node;                        //<div class="row"></div>
         this.textBox = new Div(todo, 'text-box');                  //<div class="text-box">todo</div>
-        this.completeBtn = new Button('완료', 'complete-btn');
-        this.delBtn = new Button('삭제', 'del-btn');
+        this.completeBtn = new Button('완료', 'complete-btn');     //<button class="complete-btn">완료</button>
+        this.delBtn = new Button('삭제', 'del-btn');               //<button class="del-btn">삭제</button>
     }
 
-    addRow() {
+    addRow() {                                                     //this.row의 자식으로 this.textBox, this.completeBtn, this.delBtn 넣기
         [this.textBox, this.completeBtn, this.delBtn].forEach((dom) => {
             this.row.appendChild(dom.node);
         })
         return this.row
     }
+    /*
+    addRow() return값
+
+    <div class="row">
+        <div class="text-box">todo</div>
+        <button class="complete-btn">완료</button>
+        <button class="del-btn">삭제</button>
+    </div>
+    */
+
     getRow(){
         return this.row;
     }
