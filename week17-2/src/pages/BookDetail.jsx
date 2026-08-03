7c4d4f845892ba8afeac260a47ff316193efa56d
h1gyeon9 <nhk1007@naver.com>
2026-08-03 22:02:37 +0900

    refactor: migrate BookDetail.jsx

--- week17-2/src/pages/BookDetail.jsx
+++ week17-2/src/pages/BookDetail.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import styled from 'styled-components';
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
 
@@ -39,38 +38,11 @@ const BookDetail = () => {
       <h1>{book.title}</h1>
       <h3>{book.author}</h3>
       <p>{book.description}</p>  
-      <Button onClick={updateLikes}>
-        <Icon>👍🏻</Icon> {likes}
-      </Button>
+      <button className="bg-[#75b5f5] text-white border-none rounded-[25px] py-[5px] px-[15px] text-[16px] cursor-pointer flex items-center justify-center transition-colors duration-300 ease-[ease] hover:bg-[#9ecfff] active:bg-[#3d9dfd]" onClick={updateLikes}>
+        <span className="mr-[8px] text-[20px]">👍🏻</span> {likes}
+      </button>
     </div>
   )
 }
 
 export default BookDetail
-
-const Button = styled.button`
-  background-color: #75b5f5;
-  color: #ffffff;
-  border: none;
-  border-radius: 25px;
-  padding: 5px 15px;
-  font-size: 16px;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: #9ecfff;
-  }
-
-  &:active {
-    background-color: #3d9dfd;
-  }
-`;
-
-const Icon = styled.span`
-  margin-right: 8px;
-  font-size: 20px;
-`;
